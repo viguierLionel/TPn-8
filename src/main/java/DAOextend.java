edcb54313a0c64345910e57853acc357c269bb64
pedago <pedago@pcalb-mm0411.univ-jfc.fr>
2019-10-23 16:44:46 +0200

    TP 8

---
+++ src/main/java/DAOextend.java
@@ -0,0 +1,25 @@
+
+import javax.sql.DataSource;
+import simplejdbc.DAO;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pedago
+ */
+public class DAOextend extends DAO {
+    
+    public DAOextend(DataSource dataSource) {
+        super(dataSource);
+    }
+    
+    
+
+    
+    
+}
